ed3e6555da24f177822187c8ff628d3060b4ca1e
wnill@github.com <wnill@github.com>
2016-07-27 12:06:06 +0200

    Implemented NeighborhoodSearch Sequencing and Szwarc Timing

--- Masterarbeit/src/main/java/de/wnill/master/core/scheduling/NeighborhoodSearch.java
+++ Masterarbeit/src/main/java/de/wnill/master/core/scheduling/NeighborhoodSearch.java
@@ -1,9 +1,15 @@
 package de.wnill.master.core.scheduling;
 
+import java.time.Duration;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.wnill.master.core.valuation.Valuator;
 import de.wnill.master.simulator.types.Job;
@@ -15,23 +21,118 @@ import de.wnill.master.simulator.utils.JobDueDateComparator;
  */
 public class NeighborhoodSearch implements SchedulingAlgorithm {
 
+  private static final Logger logger = LoggerFactory.getLogger(NeighborhoodSearch.class);
+  private NaiveSymetricPenalties alg = new NaiveSymetricPenalties();
+  private LocalTime earliestStart;
+  private LocalTime latestComplete;
+
   @Override
   public List<Job> scheduleJobs(List<Job> jobs, LocalTime earliestStart, LocalTime latestComplete,
       Valuator valuator) {
 
+    this.earliestStart = earliestStart;
+    this.latestComplete = latestComplete;
+
     // Start sequence is ordering by Earliest Due Date (EDD) first
     Collections.sort(jobs, new JobDueDateComparator());
 
-    List<Job> bestSequence = determineSequence(jobs, valuator);
+    List<Job> bestSequence = findBestSchedule(jobs, valuator);
+    logger.debug("best Sequence: " + bestSequence);
 
-    NaiveSymetricPenalties alg = new NaiveSymetricPenalties();
-    List<Job> result = alg.findOptimalJobTimes(bestSequence, earliestStart, latestComplete);
-
-    return result;
+    return timeJobs(bestSequence, earliestStart);
   }
 
-  private List<Job> determineSequence(List<Job> jobs, Valuator valuator) {
-    long bestValuation = valuator.getValuation(jobs);
+  /**
+   * Based on Szwarc/
+   * 
+   * @param jobs
+   * @return
+   */
+  public List<Job> timeJobs(List<Job> jobs, LocalTime earliestStart) {
+
+    LinkedList<Job> schedule = new LinkedList<>(jobs);
+    List<Integer> alphas = generatesAlphas(jobs);
+    List<Integer> betas = generateBetas(jobs);
+
+    // Step 0 - Identify clusters and caluclate related values
+    LinkedList<Cluster> clusters = initializeClusters(earliestStart, schedule, alphas, betas);
+
+    processNextCluster(clusters);
+    return schedule;
+  }
+
+  protected void processNextCluster(LinkedList<Cluster> clusters) {
+
+    if (clusters.isEmpty())
+      return;
+
+    // Step 1 - find cluster with smallest index and aggregated delta <= 0
+    int s = -1;
+    long aggregatedDelta = 0;
+    for (Cluster cluster : clusters) {
+      aggregatedDelta += cluster.getDelta();
+      if (aggregatedDelta <= 0) {
+        s = clusters.indexOf(cluster);
+        break;
+      }
+    }
+
+    if (s == clusters.size() - 1) {
+      return;
+    }
+
+    // Step 3
+    if (s == -1) {
+      // Determine minimum E
+      long minE = Long.MAX_VALUE;
+      for (Cluster cluster : clusters) {
+        if (cluster.getE() < minE) {
+          minE = cluster.getE();
+        }
+      }
+
+
+      for (Cluster cluster : clusters) {
+        // Shift each Job
+        for (Job job : cluster.getJobs()) {
+          job.setScheduledStart(job.getScheduledStart().plus(Duration.ofMinutes(minE)));
+        }
+
+        // Remove latest early jobs that are not early anymore
+        while (cluster.getLastEarlyJob() != null
+            && (cluster.getLastEarlyJob().getScheduledEnd()
+                .isAfter(cluster.getLastEarlyJob().getDue()) || cluster.getLastEarlyJob()
+                .getScheduledEnd().equals(cluster.getLastEarlyJob().getDue()))) {
+          int index = cluster.getJobs().indexOf(cluster.getLastEarlyJob());
+          if (index > 0) {
+            cluster.setLastEarlyJob(cluster.getJobs().get(index - 1));
+          } else {
+            cluster.setLastEarlyJob(null);
+          }
+        }
+        // Update E and delta
+        cluster.calculateValues();
+      }
+      processNextCluster(clusters);
+    }
+
+
+    // Step 2
+    if (s >= 0 && s < clusters.size() - 1) {
+      int toRemove = s + 1;
+      for (int i = 0; i < toRemove; i++) {
+        clusters.removeFirst();
+      }
+      processNextCluster(clusters);
+    }
+
+
+  }
+
+  private List<Job> findBestSchedule(List<Job> jobs, Valuator valuator) {
+    List<Job> initSchedule = alg.findOptimalJobTimes(jobs, earliestStart, latestComplete);
+
+    long bestValuation = valuator.getValuation(initSchedule);
     List<Job> bestSequence = jobs;
 
     for (Job job : jobs) {
@@ -46,14 +147,200 @@ public class NeighborhoodSearch implements SchedulingAlgorithm {
           LinkedList<Job> permutation = new LinkedList<>(jobs);
           Job theBreak = permutation.remove(originalPosition);
           permutation.add(position, theBreak);
-          if (valuator.getValuation(permutation) < bestValuation) {
-            bestValuation = valuator.getValuation(permutation);
-            bestSequence = permutation;
+          List<Job> schedule = alg.findOptimalJobTimes(permutation, earliestStart, latestComplete);
+          if (valuator.getValuation(schedule) < bestValuation) {
+            bestValuation = valuator.getValuation(schedule);
+            bestSequence = schedule;
           }
           position--;
         }
       }
     }
     return bestSequence;
+  }
+
+
+  private List<Integer> generateBetas(List<Job> jobs) {
+    ArrayList<Integer> betas = new ArrayList<>();
+    for (Job job : jobs) {
+      if (job.getDelivery() == null) {
+        betas.add(10000);
+      } else {
+        betas.add(10);
+      }
+    }
+
+    return betas;
+  }
+
+  private List<Integer> generatesAlphas(List<Job> jobs) {
+    ArrayList<Integer> alphas = new ArrayList<>();
+    for (Job job : jobs) {
+      if (job.getDelivery() == null) {
+        alphas.add(1);
+      } else {
+        alphas.add(5);
+      }
+    }
+
+    return alphas;
+  }
+
+  protected LinkedList<Cluster> initializeClusters(LocalTime earliestStart,
+      LinkedList<Job> schedule, List<Integer> alphas, List<Integer> betas) {
+    LinkedList<Cluster> clusters = new LinkedList<>();
+    Cluster cluster = new Cluster();
+    clusters.add(cluster);
+
+    // Step 0 - initialize
+    LocalTime startTime = earliestStart;
+    Duration earliness = Duration.ofMinutes(100000);
+    for (int i = 0; i < schedule.size(); i++) {
+      Job job = schedule.get(i);
+
+      job.setScheduledStart(startTime);
+      Duration newEarliness = Duration.between(job.getScheduledEnd(), job.getDue());
+
+      if (!earliness.isNegative() && newEarliness.isNegative() && i > 0) {
+        cluster.setLastEarlyJob(schedule.get(i - 1));
+      }
+
+      if (newEarliness.compareTo(earliness) > 0) {
+        cluster.calculateValues();
+        cluster = new Cluster();
+        clusters.add(cluster);
+      }
+      cluster.addJob(job, alphas.get(i), betas.get(i));
+      earliness = newEarliness;
+      startTime = startTime.plus(job.getDuration());
+    }
+
+    // dont forget to calculate the values of last cluster
+    cluster.calculateValues();
+
+    // Cleanup empty clusters
+    Iterator<Cluster> it = clusters.iterator();
+    while (it.hasNext()) {
+      if (it.next().getJobs().isEmpty()) {
+        it.remove();
+      }
+    }
+
+    return clusters;
+  }
+
+  public class Cluster {
+    private List<Job> jobs;
+    private List<Integer> alphas;
+    private List<Integer> betas;
+    private Job lastEarlyJob;
+    private long delta0 = 0;
+    private long E;
+    private long delta;
+
+    public Cluster() {
+      jobs = new LinkedList<>();
+      alphas = new LinkedList<>();
+      betas = new LinkedList<>();
+    }
+
+    public void calculateValues() {
+      delta0 = 0;
+      for (Integer beta : betas) {
+        delta0 -= beta;
+      }
+
+      if (lastEarlyJob == null) {
+        E = Long.MAX_VALUE;
+        delta = delta0;
+        return;
+      }
+
+      int indexOfLastEarlyJob = jobs.indexOf(lastEarlyJob);
+      delta =
+          calcDelta(indexOfLastEarlyJob - 1) + alphas.get(indexOfLastEarlyJob)
+              + betas.get(indexOfLastEarlyJob);
+
+      E = Long.MAX_VALUE;
+      for (int i = 0; i <= indexOfLastEarlyJob; i++) {
+        long newE =
+            Duration.between(jobs.get(i).getScheduledEnd(), jobs.get(i).getDue()).toMinutes();
+        if (newE < E) {
+          E = newE;
+        }
+      }
+
+    }
+
+    private long calcDelta(int i) {
+      if (i < 0) {
+        return delta0;
+      } else {
+        return calcDelta(i - 1) + alphas.get(i) + betas.get(i);
+      }
+    }
+
+    /**
+     * @return the delta0
+     */
+    public long getDelta0() {
+      return delta0;
+    }
+
+    /**
+     * @param delta0 the delta0 to set
+     */
+    public void setDelta0(long delta0) {
+      this.delta0 = delta0;
+    }
+
+    public void addJob(Job job, Integer alpha, Integer beta) {
+      jobs.add(job);
+      alphas.add(alpha);
+      betas.add(beta);
+    }
+
+    public List<Job> getJobs() {
+      return jobs;
+    }
+
+    /**
+     * @return the lastEarlyJobIndex
+     */
+    public Job getLastEarlyJob() {
+      return lastEarlyJob;
+    }
+
+    /**
+     * @param lastEarlyJobIndex the lastEarlyJobIndex to set
+     */
+    public void setLastEarlyJob(Job lastEarlyJob) {
+      this.lastEarlyJob = lastEarlyJob;
+    }
+
+    /**
+     * @return the e
+     */
+    public long getE() {
+      return E;
+    }
+
+    /**
+     * @return the delta
+     */
+    public long getDelta() {
+      return delta;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+      return "Cluster [jobs=" + jobs + ", lastEarlyJob=" + lastEarlyJob + ", delta0=" + delta0
+          + "]";
+    }
   }
 }
--- Masterarbeit/test/de/wnill/master/core/scheduling/NeighborhoodSearchTest.java
+++ Masterarbeit/test/de/wnill/master/core/scheduling/NeighborhoodSearchTest.java
@@ -1,5 +1,8 @@
 package de.wnill.master.core.scheduling;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -8,6 +11,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import de.wnill.master.core.scheduling.NeighborhoodSearch.Cluster;
 import de.wnill.master.core.scheduling.viz.ScheduleVisualizer;
 import de.wnill.master.core.valuation.NonMonotonicLatenessValuation;
 import de.wnill.master.core.valuation.Valuator;
@@ -68,4 +72,158 @@ public class NeighborhoodSearchTest {
 
   }
 
+  @SuppressWarnings("deprecation")
+  @Test
+  public void testClusterGeneration() {
+    LinkedList<Job> jobs = new LinkedList<>();
+
+    Job job = new Job(LocalTime.of(0, 12), Duration.ofMinutes(3));
+    job.setId("1");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 4), Duration.ofMinutes(2));
+    job.setId("2");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 26), Duration.ofMinutes(7));
+    job.setId("3");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 18), Duration.ofMinutes(3));
+    job.setId("4");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 16), Duration.ofMinutes(6));
+    job.setId("5");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 25), Duration.ofMinutes(2));
+    job.setId("6");
+    jobs.add(job);
+
+    job = new Job(LocalTime.of(0, 30), Duration.ofMinutes(8));
+    job.setId("7");
+    jobs.add(job);
+
+    List<Integer> alphas = Arrays.asList(10, 20, 18, 9, 10, 16, 11);
+    List<Integer> betas = Arrays.asList(12, 25, 38, 12, 12, 18, 15);
+
+
+    NeighborhoodSearch alg = new NeighborhoodSearch();
+    List<Cluster> clusters = alg.initializeClusters(LocalTime.of(0, 0), jobs, alphas, betas);
+
+    assertEquals(3, clusters.size());
+    assertTrue(clusters.get(0).getJobs().get(0).getId().equals("1"));
+    assertTrue(clusters.get(0).getJobs().get(0).getScheduledEnd().equals(LocalTime.of(0, 3)));
+    assertTrue(clusters.get(0).getJobs().get(1).getId().equals("2"));
+    assertTrue(clusters.get(0).getJobs().get(1).getScheduledEnd().equals(LocalTime.of(0, 5)));
+    assertTrue(clusters.get(1).getJobs().get(0).getId().equals("3"));
+    assertTrue(clusters.get(1).getJobs().get(0).getScheduledEnd().equals(LocalTime.of(0, 12)));
+    assertTrue(clusters.get(1).getJobs().get(1).getId().equals("4"));
+    assertTrue(clusters.get(1).getJobs().get(1).getScheduledEnd().equals(LocalTime.of(0, 15)));
+    assertTrue(clusters.get(1).getJobs().get(2).getId().equals("5"));
+    assertTrue(clusters.get(1).getJobs().get(2).getScheduledEnd().equals(LocalTime.of(0, 21)));
+    assertTrue(clusters.get(2).getJobs().get(0).getId().equals("6"));
+    assertTrue(clusters.get(2).getJobs().get(0).getScheduledEnd().equals(LocalTime.of(0, 23)));
+    assertTrue(clusters.get(2).getJobs().get(1).getId().equals("7"));
+    assertTrue(clusters.get(02).getJobs().get(1).getScheduledEnd().equals(LocalTime.of(0, 31)));
+
+    assertEquals("1", clusters.get(0).getLastEarlyJob().getId());
+    assertEquals("4", clusters.get(1).getLastEarlyJob().getId());
+    assertEquals("6", clusters.get(2).getLastEarlyJob().getId());
+
+    assertEquals(9, clusters.get(0).getE());
+    assertEquals(3, clusters.get(1).getE());
+    assertEquals(2, clusters.get(2).getE());
+
+    assertEquals(-15, clusters.get(0).getDelta());
+    assertEquals(15, clusters.get(1).getDelta());
+    assertEquals(1, clusters.get(2).getDelta());
+  }
+
+  @Test
+  public void testClusterProcessing() {
+    LinkedList<Job> jobs = new LinkedList<>();
+
+    Job job = new Job(LocalTime.of(0, 12), Duration.ofMinutes(3));
+    job.setId("1");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 4), Duration.ofMinutes(2));
+    job.setId("2");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 26), Duration.ofMinutes(7));
+    job.setId("3");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 18), Duration.ofMinutes(3));
+    job.setId("4");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 16), Duration.ofMinutes(6));
+    job.setId("5");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 25), Duration.ofMinutes(2));
+    job.setId("6");
+    jobs.add(job);
+    job = new Job(LocalTime.of(0, 30), Duration.ofMinutes(8));
+    job.setId("7");
+    jobs.add(job);
+
+    List<Integer> alphas = Arrays.asList(10, 20, 18, 9, 10, 16, 11);
+    List<Integer> betas = Arrays.asList(12, 25, 38, 12, 12, 18, 15);
+    NeighborhoodSearch alg = new NeighborhoodSearch();
+    LinkedList<Cluster> clusters = alg.initializeClusters(LocalTime.of(0, 0), jobs, alphas, betas);
+
+    alg.processNextCluster(clusters);
+
+  }
+
+
+  @Test
+  public void testTimeJobs() {
+    LinkedList<Job> jobList = new LinkedList<>();
+    Job job1 =
+        new Job(new Delivery(2, LocalTime.of(0, 12)), LocalTime.of(0, 12), Duration.ofMinutes(10));
+    job1.setId("1");
+
+    Job job2 =
+        new Job(new Delivery(1, LocalTime.of(0, 10)), LocalTime.of(0, 10), Duration.ofMinutes(5));
+    job2.setId("2");
+
+    Job job3 =
+        new Job(new Delivery(3, LocalTime.of(0, 20)), LocalTime.of(0, 20), Duration.ofMinutes(3));
+    job3.setId("3");
+
+    Job job4 =
+        new Job(new Delivery(4, LocalTime.of(0, 23)), LocalTime.of(0, 23), Duration.ofMinutes(3));
+    job4.setId("4");
+
+
+    jobList.add(job1);
+    jobList.add(job2);
+    jobList.add(job3);
+    jobList.add(job4);
+
+    jobList.add(new Job(LocalTime.of(0, 17), Duration.ofMinutes(5)));
+
+    NeighborhoodSearch alg = new NeighborhoodSearch();
+    List<Job> result =
+        alg.scheduleJobs(jobList, LocalTime.of(0, 0), LocalTime.of(1, 0),
+            new NonMonotonicLatenessValuation());
+
+
+    Valuator val = new NonMonotonicLatenessValuation();
+
+    Truck truck = new Truck(1, new NeighborhoodSearch(), new NonMonotonicLatenessValuation());
+    truck.setSchedule(result);
+    ScheduleVisualizer viz = new ScheduleVisualizer(Arrays.asList(truck));
+
+    System.out.println(val.getValuation(result));
+
+    try {
+      Thread.sleep(500000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
 }
